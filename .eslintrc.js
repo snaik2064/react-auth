1106eb07027e3e0ecf69c4b5de79d2879e2c07ae
Shashank <snaik2064@gmail.com>
2021-12-22 11:40:51 +0530

    [New]: Added Prettier-formatter & eslint config files.

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  extends: ['airbnb', 'airbnb/hooks', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-props-no-spreading': 'off',
+    'no-use-before-define': [
+      'error',
+      { functions: true, classes: true, variables: false },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'no-prototype-builtins': 'off',
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'function-declaration',
+      },
+    ],
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+};
