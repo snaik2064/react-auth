371b88564a7b2b97ac0b55b041226e2047e23527
Shashank <snaik2064@gmail.com>
2021-12-28 16:28:49 +0530

    [Feature]: Route configuration added with react-router.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import { SignUp } from './components/auth';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+
+import { PATH_SIGN_UP, PATH_SIGN_IN } from './constants/RouteConstants';
+import { SignUp, SignIn } from './components/auth';
 
 function App() {
   return (
     <div className="App">
-      <SignUp />
+      <BrowserRouter>
+        <Routes>
+          <Route path={PATH_SIGN_IN} element={<SignIn />} />
+          <Route path={PATH_SIGN_UP} element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/constants/RouteConstants.js
@@ -0,0 +1,5 @@
+export const PATH_ROOT = '/';
+
+// Auth routes
+export const PATH_SIGN_IN = '/sign-in';
+export const PATH_SIGN_UP = '/sign-up';
